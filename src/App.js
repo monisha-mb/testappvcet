9c081f2b8202f748efaf616681182e773c083528
HEMASIVA22 <shemasivanesan@gmail.com>
2023-11-01 15:55:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Navigation from './Navagation/Navigation';
+import Menubar from './Components/Menubar';
+import Aboutus from './Components/Aboutus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contacts" element={<Contact/>} />
+          <Route path="/menubar" element={<Menubar/>} />
+          <Route path="/aboutus" element={<Aboutus/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
